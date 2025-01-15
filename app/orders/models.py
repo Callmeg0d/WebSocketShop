df3d2775f66e61cc6cf76d395255792dad4cf2f9
Callmegod <125900081+Callmeg0d@users.noreply.github.com>
2025-01-15 17:35:59 +0300

    Пофикшены недочеты

--- app/orders/models.py
+++ app/orders/models.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from app.database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey, Date, JSON
 from datetime import date
+
+if TYPE_CHECKING:
+    from app.users.models import Users
 
 
 class Orders(Base):
@@ -9,10 +14,10 @@ class Orders(Base):
 
     order_id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    created_at: Mapped[date] = mapped_column(Date, nullable=False)
-    status: Mapped[str] = mapped_column(nullable=False)
-    delivery_address: Mapped[str] = mapped_column(nullable=False)
-    order_items: Mapped[list[str]] = mapped_column(JSON, nullable=False)
-    total_cost: Mapped[int] = mapped_column(nullable=False)
+    created_at: Mapped[date] = mapped_column(Date)
+    status: Mapped[str] = mapped_column()
+    delivery_address: Mapped[str] = mapped_column()
+    order_items: Mapped[list[str]] = mapped_column(JSON)
+    total_cost: Mapped[int] = mapped_column()
 
     user: Mapped["Users"] = relationship(back_populates="orders")
