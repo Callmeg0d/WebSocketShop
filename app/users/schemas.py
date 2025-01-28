1fd6b097d52f9856798fe4f3f23525f535b557e2
Callmegod <125900081+Callmeg0d@users.noreply.github.com>
2025-01-28 22:13:25 +0300

    Удалено поле name в схеме SUserAuth

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel, EmailStr, ConfigDict
 
 class SUserAuth(BaseModel):
     email: EmailStr
-    name: str
     password: str
 
     model_config = ConfigDict(from_attributes=True)
