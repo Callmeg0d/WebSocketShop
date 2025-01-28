992c479d0e372d64385a7f5dcca9693e730fa744
Callmegod <125900081+Callmeg0d@users.noreply.github.com>
2025-01-28 22:10:12 +0300

    Добавлен эндпоинт для выхода из профиля. Изменены входные параметры использования UsersDAO.add, добавлен 201 ответ при успешной регистрации

--- app/users/router.py
+++ app/users/router.py
@@ -1,29 +1,37 @@
-from fastapi import APIRouter
-from fastapi import Response
+from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Response
 from app.users.auth import get_password_hash, create_access_token
 from app.users.schemas import SUserAuth
 from app.users.dao import UsersDAO
 from app.users.auth import authenticate_user
-from app.exceptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException
+from app.exceptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException, CannotAddDataToDatabase
 
 
-router = APIRouter(
+router_auth = APIRouter(
     prefix="/auth",
-    tags=["Auth & Пользователи"]
+    tags=["Auth"]
+)
+
+router_users = APIRouter(
+    prefix="/users",
+    tags=["Пользователи"],
 )
 
 
-@router.post("/register")
+@router_auth.post('/register')
 async def register_user(user_data: SUserAuth):
+    # Проверяем существование юзера
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
         raise UserAlreadyExistsException
 
     hashed_password = get_password_hash(user_data.password)
-    await UsersDAO.add(email=user_data.email, name=user_data.name, hashed_password=hashed_password)
+    await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
+
+    return JSONResponse(content={"message": "User created successfully"}, status_code=201)
 
 
-@router.post('/login')
+@router_auth.post('/login')
 async def login_user(response: Response, user_data: SUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
@@ -32,3 +40,8 @@ async def login_user(response: Response, user_data: SUserAuth):
     #Помещаем созданный токен в куки
     response.set_cookie("access_token", access_token, max_age=1800, httponly=True) #max_age для удаления куков, при истичении действия expire(в сек.)
     return {"access_token": access_token}
+
+
+@router_auth.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie("access_token")
