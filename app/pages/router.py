f4f227c623e40c3147899620b200094fa96ca007
Callmegod <125900081+Callmeg0d@users.noreply.github.com>
2025-01-28 22:00:17 +0300

    Добавлены эндпоинты для страниц регистрации и входа

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
-
+from fastapi.responses import HTMLResponse
 from app.products.router import get_products
 
 router = APIRouter(
@@ -10,6 +10,16 @@ router = APIRouter(
 )
 
 templates = Jinja2Templates(directory="app/templates")
+
+
+@router.get("/login", response_class=HTMLResponse)
+async def get_login_page(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+
+@router.get("/register", response_class=HTMLResponse)
+async def get_register_page(request: Request):
+    return templates.TemplateResponse("auth/register.html", {"request": request})
 
 
 @router.get("/products")
