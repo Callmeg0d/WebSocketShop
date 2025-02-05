de80e17851952cde1e6f43413474ed6f0d970b98
Egor Muratov <egor.muratov.27@mail.ru>
2025-02-05 17:00:19 +0300

    Изменена pydantic схема корзины

--- app/shopping_carts/schemas.py
+++ app/shopping_carts/schemas.py
@@ -4,5 +4,6 @@ from pydantic import BaseModel, ConfigDict
 class SShoppingCart(BaseModel):
     product_id: int
     user_id: int
+    quantity: int
 
     model_config = ConfigDict(from_attributes=True)
