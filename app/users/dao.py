18341217ffb0dac65730b5a9b4c4d5cb98bf877e
Egor Muratov <egor.muratov.27@mail.ru>
2025-02-19 20:59:13 +0300

    Метод add изменён и перенесён в класс UsersDAO

--- app/users/dao.py
+++ app/users/dao.py
@@ -1,6 +1,16 @@
 from app.dao.base import BaseDAO
+from app.database import async_session_maker
 from app.users.models import Users
 
 
 class UsersDAO(BaseDAO):
     model = Users
+
+    @classmethod
+    async def add(cls, **data):
+        async with async_session_maker() as session:
+            new_user = cls.model(**data)
+            session.add(new_user)
+            await session.commit()
+            await session.refresh(new_user)
+            return new_user
