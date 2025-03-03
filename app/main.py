9ea614b625ced75fbcf881749473e1311e57f4a7
Egor Muratov <egor.muratov.27@mail.ru>
2025-03-03 22:08:41 +0300

    Добавлены подключение веб-сокетов, их реализация на странице products, подключён новый роутер для информации о пользователе

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from app.products.router import router as router_products
-from app.users.router import router_auth as router_users
+from app.users.router import router_auth as router_users_auth
+from app.users.router import router_users as router_users
 from app.orders.router import router as router_orders
 from app.shopping_carts.router import router as router_shopping_carts
 from app.pages.router import router as router_pages
@@ -13,6 +14,8 @@ from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 from contextlib import asynccontextmanager
 from app.reviews.router import router as router_reviews
+from app.websockets import manager
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -24,6 +27,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="app/static"), "static")
 
+app.include_router(router_users_auth)
 app.include_router(router_users)
 app.include_router(router_products)
 app.include_router(router_orders)
@@ -31,7 +35,6 @@ app.include_router(router_shopping_carts)
 app.include_router(router_reviews)
 app.include_router(router_pages)
 app.include_router(router_images)
-
 # Подключение CORS, чтобы запросы к API могли приходить из браузера
 origins = [
     "http://127.0.0.1:8000",
@@ -47,3 +50,13 @@ app.add_middleware(
                    "Access-Control-Allow-Origin",
                    "Authorization"],
 )
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
