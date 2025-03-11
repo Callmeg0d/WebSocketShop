18de013ee4ecd3dd5dde20cbe17e8cbccc4a1a37
Egor Muratov <egor.muratov.27@mail.ru>
2025-03-11 22:34:53 +0300

    Добавлены тесты на основные функции dao и api, настройка окружения для тестирования и тестовые данные

---
+++ app/tests/conftest.py
@@ -0,0 +1,80 @@
+import asyncio
+import json
+from datetime import datetime
+
+import pytest
+from sqlalchemy import insert, text
+
+from app.categories.models import Categories
+from app.config import settings
+from app.database import Base, engine, async_session_maker
+from app.orders.models import Orders
+from app.main import app as fastapi_app
+from app.products.models import Products
+from app.reviews.models import Reviews
+from app.shopping_carts.models import ShoppingCarts
+
+from app.users.models import Users
+
+from httpx import AsyncClient, ASGITransport
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_database():
+    assert settings.MODE == "TEST"
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    def open_test_file(model: str):
+        with open(f"app/tests/mock_{model}.json", encoding="utf-8") as file:
+            return json.load(file)
+
+    users = open_test_file("users")
+    categories = open_test_file("categories")
+    products = open_test_file("products")
+    shopping_carts = open_test_file("shopping_carts")
+    orders = open_test_file("orders")
+    reviews = open_test_file("reviews")
+
+    for order in orders:
+        # Алхимия не принимает дату в текстовом формате, поэтому форматируем к datetime
+        order["created_at"] = datetime.strptime(order["created_at"], "%Y-%m-%d")
+
+    async with async_session_maker() as session:
+        for Model, values in [
+            (Users, users),
+            (Categories, categories),
+            (Products, products),
+            (ShoppingCarts, shopping_carts),
+            (Orders, orders),
+            (Reviews, reviews)
+        ]:
+            query = insert(Model).values(values)
+            await session.execute(query)
+
+        await session.commit()
+
+
+@pytest.fixture(scope="function")
+async def async_client():
+    async with AsyncClient(transport=ASGITransport(fastapi_app), base_url="http://test") as ac:
+        yield ac
+
+
+@pytest.fixture
+async def authenticated_user(async_client: AsyncClient):
+    login_data = {"email": "test2@test2.com", "password": "test2"}
+
+    response = await async_client.post("/auth/login", json=login_data)
+    assert response.status_code == 200
+
+    tokens = response.json()
+    return {"access_token": tokens["access_token"]}
+
+
+@pytest.fixture
+def auth_headers(authenticated_user):
+    """Фикстура для заголовков авторизации"""
+    return {"Authorization": f"Bearer {authenticated_user['access_token']}"}
---
+++ app/tests/products_tests/test_products_dao.py
@@ -0,0 +1,43 @@
+import ast
+import json
+
+from httpx import AsyncClient
+
+import pytest
+
+from app.database import async_session_maker
+from app.products.dao import ProductDAO
+from app.products.models import Products
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "product_id, expected_product",
+    [
+        (1, {"product_id": 1, "name": "iPhone 13",
+             "description": "Новый смартфон от Apple с A15 Bionic и 128 ГБ памяти.", "price": 79990,
+             "product_quantity": 3, "image": 1,
+             "features": {'Вес': '173 г', 'Память': '128 ГБ', 'Размеры': '146.7 x 71.5 x 7.65 мм'},
+             "category_name": "Телефоны"}),
+        (999, None),
+    ],
+)
+async def test_get_product_by_id(product_id, expected_product, async_client: AsyncClient):
+    product_data = await ProductDAO.get_product_by_id(product_id)
+
+    if expected_product:
+        assert product_data is not None
+        for key, value in expected_product.items():
+            if key == "features":
+                product_features = product_data[key]
+
+                if isinstance(product_features, str):
+                    try:
+                        product_features = ast.literal_eval(product_features)  # преобразование строки в словарь
+                    except (SyntaxError, ValueError):
+                        assert False
+
+                assert isinstance(product_features, dict)
+                assert sorted(product_features.items()) == sorted(value.items())
+            else:
+                assert product_data[key] == value
